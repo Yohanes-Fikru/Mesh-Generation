3c72add63cc8aa0e347bcc297f33c818206bd7c3
MajorJohn <yohanes.fikru@gmail.com>
2022-03-18 15:10:09 +0300

    upgraded to 2020.31f1

---
+++ Assets/Scripts/AdvancedMultiStreamProceduralMesh.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class AdvancedMultiStreamProceduralMesh : MonoBehaviour
+{
+    
+    
+    void OnEnable () {
+        var mesh = new Mesh {
+            name = "Procedural Mesh"
+        };
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+}
